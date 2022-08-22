bc6be9ef9b5244bcafb735e011034c1a20291f4f
S. McCarthy <90977936+Smccarthy17@users.noreply.github.com>
2022-08-21 21:02:40 -0400

    finished employee data and team array

--- index.js
+++ index.js
@@ -150,3 +150,31 @@ const addEmployee = () => {
                 }
             }
         },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Do you want to add more team members?',
+            default: false
+        }
+    ])
+    .then(employeeData => {
+        let { name, id, email, role, github, school, confirmAddEmployee } = employeeData; 
+        let employee; 
+
+        if (role === "Engineer") {
+            employee = new Engineer (name, id, email, github);
+            console.log(employee);
+        } else if (role === "Intern") {
+            employee = new Intern (name, id, email, school);
+            console.log(employee);
+        }
+        teamArray.push(employee); 
+        if (confirmAddEmployee) {
+            return addEmployee(teamArray); 
+        } else {
+            return teamArray;
+        }
+    })
+};
+
+
